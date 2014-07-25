4babab312cae88cd8c414cb7d7b4dff0be5926dc
quido3 <koippalainen@hotmail.com>
2014-07-25 17:50:43 +0300

    Changes to graphics and musics
    Changes

---
+++ Assets/Scripts/jumpScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jumpScript : MonoBehaviour
+{
+
+    Vector3 origSpot = Vector3.zero;
+
+    public float jumpForce = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        origSpot = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= origSpot.y)
+        {
+            rigidbody2D.velocity = Vector3.zero;
+            Vector2 jump = new Vector2(0, jumpForce);
+            this.rigidbody2D.AddForce(jump);
+        }
+    }
+}
