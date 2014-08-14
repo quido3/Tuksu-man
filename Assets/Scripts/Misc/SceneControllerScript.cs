a67468066b926fbbe43b09af334fc2d7dfc347ef
quido3 <koippalainen@hotmail.com>
2014-08-14 14:44:04 +0300

    Muchos thingos
    Do not remember. Possibly the latest version that went to iTunes

---
+++ Assets/Scripts/Misc/SceneControllerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneControllerScript : MonoBehaviour
+{
+    //Hopefully this fixes the 30fps on iPad
+    // Use this for initialization
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/UI/flotAway.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flotAway : MonoBehaviour
+{
+
+    public bool left = false;
+    public GameObject shadow;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color c = shadow.GetComponent<SpriteRenderer>().color;
+        c.a = c.a - 0.05f;
+        shadow.GetComponent<SpriteRenderer>().color = c;
+        Vector2 force = Vector2.zero;
+        if (left == true)
+        {
+            force.x = -30;
+        }
+        else
+        {
+            force.x = 30;
+        }
+        this.rigidbody2D.AddForce(force);
+
+        if (this.transform.position.x < -10 || this.transform.position.x > 20)
+        {
+            Destroy(this);
+        }
+    }
+}
